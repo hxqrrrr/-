cbceef96d01cbd082d2f1dc77b36c8ba5a3e5aca
hxq <hxq11115@gmail.com>
2024-07-13 20:48:44 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   "2-3\346\234\200\345\260\217\347\224\237\346\210\220\346\240\221/\344\273\243\347\240\201.py"

--- 2-3最小生成树/代码.py
+++ 2-3最小生成树/代码.py
@@ -1,12 +1,17 @@
 import numpy as np
 from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import minimum_spanning_tree
+from scipy.sparse.csgraph import minimum_spanning_tree, dijkstra
 
 # 创建一个加权邻接矩阵
-adj_matrix = np.array([[0, 6, 1, 5],
-                       [6, 0, 5, 3],
-                       [1, 5, 0, 6],
-                       [5, 3, 6, 0]])
+adj_matrix = np.array([[0 , 2 , 1 , 3 , 4 , 4 , 2 , 5 , 4 ],
+                       [2 , 0 , 4 , 0 , 0 , 0 , 0 , 0 , 1 ],
+                       [1 , 4 , 0 , 1 , 0 , 0 , 0 , 0 , 0 ],
+                       [3 , 0 , 1 , 0 , 1 , 0 , 0 , 0 , 0 ],
+                       [4 , 0 , 0 , 1 , 0 , 5 , 0 , 0 , 0 ],
+                       [4 , 0 , 0 , 0 , 5 , 0 , 2 , 0 , 0 ],
+                       [2 , 0 , 0 , 0 , 0 , 2 , 0 , 3 , 0 ],
+                       [5 , 0 , 0 , 0 , 0 , 0 , 3 , 0 , 5 ],
+                       [4 , 1 , 0 , 0 , 0 , 0 , 0 , 5 , 0 ],])
 
 # 将邻接矩阵转换为稀疏矩阵
 adj_sparse = csr_matrix(adj_matrix)
@@ -15,4 +20,15 @@ adj_sparse = csr_matrix(adj_matrix)
 mst = minimum_spanning_tree(adj_sparse)
 
 # 输出最小生成树的邻接矩阵
+print("最小生成树的邻接矩阵：")
 print(mst.toarray().astype(int))
+
+# 计算最短路径
+dist_matrix, predecessors = dijkstra(csgraph=adj_sparse, return_predecessors=True)
+
+# 输出最短路径的距离矩阵和前驱矩阵
+print("\n最短路径的距离矩阵：")
+print(dist_matrix)
+
+print("\n前驱矩阵：")
+print(predecessors)
